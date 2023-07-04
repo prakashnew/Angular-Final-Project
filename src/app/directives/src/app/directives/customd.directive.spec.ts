4ecd0b47666d4c453aafa8ac9b4552313427bbc3
prakashnew <134586332+prakashnew@users.noreply.github.com>
2023-07-04 12:25:10 +0530

    Delete src/app/directives/src/app/directives directory

--- src/app/directives/src/app/directives/customd.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomdDirective } from './customd.directive';
-
-describe('CustomdDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CustomdDirective();
-    expect(directive).toBeTruthy();
-  });
-});
